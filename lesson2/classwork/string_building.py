c15a3c78007ee3a2a9869072085411534549cdb7
Kaya-12345 <purpledog13131@gmail.com>
2026-04-17 17:25:55 -0700

    Update string_building.py

--- lesson2/classwork/string_building.py
+++ lesson2/classwork/string_building.py
@@ -11,6 +11,14 @@ result = ""
 for ch in letters:
   result = result + ch
 print(result)
+
 # Smart way
 built = "".join(letters)
 print(built)
+
+word = "computer"
+new_word = ""
+for i in range(len(word)):
+  if i % 2 == 0:
+    new_word = new_word + word[1]
+print(new_word)
