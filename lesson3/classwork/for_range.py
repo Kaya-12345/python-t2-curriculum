52993771145666e069c00f8a2fdd368d2301fbd5
Kaya-12345 <purpledog13131@gmail.com>
2026-05-01 17:10:14 -0700

    Update for_range.py

---
+++ lesson3/classwork/for_range.py
@@ -0,0 +1,16 @@
+for i in range (5): # range(n) goes from 0 up to n-1
+	print(i)
+
+print(“End of range(5)”)
+
+# range(start, stop)  includes start but NOT stop
+for i in range(3, 8):
+	print(i)
+
+print(“End of range(3, 8)”)
+
+for i in range(5, 11):
+	print(i)
+
+nums = list(range(1, 6))
+print(nums)
