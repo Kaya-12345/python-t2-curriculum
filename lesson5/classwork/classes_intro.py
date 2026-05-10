7a8946875948dad516c6dc5bed43faafc917d8b7
Kaya-12345 <purpledog13131@gmail.com>
2026-05-10 16:56:34 -0700

    Update classes_intro.py

---
+++ lesson5/classwork/classes_intro.py
@@ -0,0 +1,14 @@
+class Student:
+	def _init_(self, name, grad):
+		self.name = name
+		self.grade = grade
+
+def introduce(self):
+	print(“Hi, my name is”, self.name)
+	print(“I am in grad”, self.grade)
+
+student1 = Student(“Arav”, 5)
+student1.introduce()
+
+student2 = Student(Toby, 7)
+student2.introduce()
