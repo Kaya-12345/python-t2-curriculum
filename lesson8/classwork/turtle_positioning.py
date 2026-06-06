66f9017dfc72452eea53c8d8cf4cfd1a9db69fc1
Kaya-12345 <purpledog13131@gmail.com>
2026-06-05 17:10:38 -0700

    Update turtle_positioning.py

---
+++ lesson8/classwork/turtle_positioning.py
@@ -0,0 +1,24 @@
+import turtle
+
+t = turtle.Turtle()
+t.speed(7)
+
+# Move without drawing
+t.penup()
+t.goto(-200,0)
+t.pendown()
+
+t.setheading(270)
+t.circle(40,180)
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+t.shape("turtle")
+for i in range(6):
+  t.stamp()
+  t.forward(50)
+  t.left(60)
+  
+turtle.done()
